d08258a134ee6deffb6858f198960a8f11af8ba9
Nat Geyzen <nat.geyzen@outlook.com>
2020-07-13 12:27:45 -0500

    new Canvas folder with Canvas.js and Canvas.css files, very basic setup of canvas inside React component, render Canvas when Canvas button is clicked inside App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Canvas from './Canvas/Canvas';
+
 function App() {
+
+  const [ renderButton, setRenderButton ] = useState(true);
+  const [ renderCanvas, setRenderCanvas ] = useState(false);
+
+
   return (
     <div className="App">
-      <a href="#">Canvas</a>
+      {renderButton && <button onClick={() => {
+        setRenderButton(false);
+        setRenderCanvas(true);
+      }}>Canvas</button>}
+      {renderCanvas && <Canvas />}
     </div>
   );
 }
---
+++ src/Canvas/Canvas.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Canvas.css';
+
+const Canvas = () => {
+
+    return (
+        <canvas width="100" height="100" className="canvas">test</canvas>
+    )
+
+};
+
+export default Canvas;
